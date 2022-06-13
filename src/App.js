8683ba58df6e70b4978be0d3968e5c55de8737ac
kjin <kjin@sysurban.com>
2022-06-13 16:24:35 +0900

    App.js 오타 수정

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className={styles.title}>Welcom back!</h1>
+        <h1 className={styles.title}>Welcome back!</h1>
       </header>
       <div>{count}</div>
       <Button text={"continue"} onClick={onClick}/>
